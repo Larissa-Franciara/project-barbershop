c44a0ef636d49b21c57aa8306d7e2a680229cfac
Larissa Franciara <larissafranciaradealmeida@gmail.com>
2024-01-30 17:26:57 -0300

    feat: add componet search

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import Search from './_components/search';
 
 
 export default function Home() {
@@ -8,13 +9,17 @@ export default function Home() {
     <div>
       <Header />
 
-      <div className='px-5 pt-5'>
-      <h2 className='text-xl font-bold'>Olá, Larissa!</h2>
+      <div className="px-5 pt-5">
+      <h2 className="text-xl font-bold">Olá, Larissa!</h2>
       <p className="capitalize text-sm">
         {format(new Date(), "EEEE',' dd 'de' MMMM", {
           locale: ptBR,
         })}
       </p>
+      </div>
+
+      <div className="p-4">
+      <Search />
       </div>
     </div>
   )
