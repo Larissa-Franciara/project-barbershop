53e7b944226e3e4770009d76f577fb7b926d3fa6
Larissa Franciara <larissafranciaradealmeida@gmail.com>
2024-02-01 19:15:19 -0300

    fix: get day booking to get the barbershop ID

--- app/barbershops/[id]/_actions/get-day-bookings.ts
+++ app/barbershops/[id]/_actions/get-day-bookings.ts
@@ -2,10 +2,11 @@
 import { db } from "@/app/_lib/prisma";
 import { endOfDay, startOfDay } from "date-fns";
 
-export const getDayBookings = async (date: Date) => {
+export const getDayBookings = async (barbershopId: string, date: Date) => {
 
     const bookings = await db.booking.findMany({
         where: {
+            barbershopId,
             date: {
                 lte: endOfDay(date),
                 gte: startOfDay(date)
