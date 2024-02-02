5f25b488c9f45885ace029ae78c6f34ee987b5f0
Larissa Franciara <larissafranciaradealmeida@gmail.com>
2024-02-02 19:18:58 -0300

    fix: ajuste user name home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, {session?.user?.name}!</h2>
+        <h2 className="text-xl font-bold">{session?.user ? `Olá ${session.user.name?.split(" ")[0]}!` : 'Olá, visitante!'}</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
